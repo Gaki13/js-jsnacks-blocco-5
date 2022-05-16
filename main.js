c80c5066a658bf5340c5530fec3ed9a14d5edcbe
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-16 12:25:40 +0200

    creo un'array con 10 oggetti

---
+++ main.js
@@ -0,0 +1,58 @@
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+
+const zucchine = [
+    zucchinaUno = {
+        tipo : "zucchina 1",
+        peso : 50,
+        lunghezza : 20,
+    },
+    zucchinaUno = {
+        tipo : "zucchina 2",
+        peso : 50,
+        lunghezza : 20,
+    },
+    zucchinaUno = {
+        tipo : "zucchina 3",
+        peso : 50,
+        lunghezza : 20,
+    },
+    zucchinaUno = {
+        tipo : "zucchina 4",
+        peso : 50,
+        lunghezza : 20,
+    },
+    zucchinaUno = {
+        tipo : "zucchina 5",
+        peso : 50,
+        lunghezza : 20,
+    },
+    zucchinaUno = {
+        tipo : "zucchina 6",
+        peso : 50,
+        lunghezza : 20,
+    },
+    zucchinaUno = {
+        tipo : "zucchina 7",
+        peso : 50,
+        lunghezza : 20,
+    },
+    zucchinaUno = {
+        tipo : "zucchina 8",
+        peso : 50,
+        lunghezza : 20,
+    },
+    zucchinaUno = {
+        tipo : "zucchina 9",
+        peso : 50,
+        lunghezza : 20,
+    },
+    zucchinaUno = {
+        tipo : "zucchina 10",
+        peso : 50,
+        lunghezza : 20,
+    }
+];
+
+const {peso} = zucchine;
+
