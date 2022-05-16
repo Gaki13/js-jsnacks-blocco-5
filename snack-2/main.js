c8a110e2ec47ee312ade33aabd4b1bbc95d15297
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-16 12:45:17 +0200

    snack-2

---
+++ snack-2/main.js
@@ -0,0 +1,58 @@
+// Crea 10 oggetti che rappresentano una zucchina.
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+
+
+const zucchine = [
+    {
+        tipo : "zucchina 1",
+        peso : 50,
+        lunghezza : 20,
+    },
+    {
+        tipo : "zucchina 2",
+        peso : 50,
+        lunghezza : 20,
+    },
+    {
+        tipo : "zucchina 3",
+        peso : 50,
+        lunghezza : 10,
+    },
+    {
+        tipo : "zucchina 4",
+        peso : 50,
+        lunghezza : 20,
+    },
+    {
+        tipo : "zucchina 5",
+        peso : 50,
+        lunghezza : 10,
+    },
+    {
+        tipo : "zucchina 6",
+        peso : 50,
+        lunghezza : 20,
+    },
+    {
+        tipo : "zucchina 7",
+        peso : 50,
+        lunghezza : 10,
+    },
+    {
+        tipo : "zucchina 8",
+        peso : 50,
+        lunghezza : 20,
+    },
+    {
+        tipo : "zucchina 9",
+        peso : 50,
+        lunghezza : 10,
+    },
+    {
+        tipo : "zucchina 10",
+        peso : 50,
+        lunghezza : 20,
+    }
+];
